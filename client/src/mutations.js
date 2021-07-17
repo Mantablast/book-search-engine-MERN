5ee658625d8e06d02bae5eb881306b766a7fe191
AimeeJay <ajesso@post.com>
2021-07-16 22:13:57 -0400

    added mutations on front end, user query not working. saving the working queries

--- client/src/mutations.js
+++ client/src/mutations.js
@@ -2,4 +2,42 @@
 // ADD_USER will execute the addUser mutation.
 // SAVE_BOOK will execute the saveBook mutation.
 // REMOVE_BOOK will execute the removeBook mutation.
+import gql from "graphql-tag";
 
+//save and delete books from list,  similar to the get me query
+export const SAVE_BOOK = gql`
+    mutation saveBook($book: SavedBookInput!) {
+        saveBook(book: $book) {
+            username
+            email
+            bookCount
+
+            savedBooks {
+                authors
+                description
+                bookId
+                image
+                title
+                link
+            }
+        }
+    }
+`;
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            username
+            email
+            bookCount
+
+            savedBooks {
+                authors
+                description
+                bookId
+                image
+                title
+                link
+            }
+        }
+    }
+`;
