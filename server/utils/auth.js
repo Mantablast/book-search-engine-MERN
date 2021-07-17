80f932646d8ca9b8ddbfe79c2e44e98a3c03a6e8
AimeeJay <ajesso@post.com>
2021-07-16 22:58:38 -0400

    bcs assistant recommended to use babel, saving before changes

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,38 +1,34 @@
-//***referenced from lesson 21 part 6***
-import decode from "jwt-decode";
-class AuthService {
-  getProfile() {
-    return decode(this.getToken());
-  }
-  loggedIn() {
-    // checks if saved token and it's still valid
-    const token = this.getToken();
-    return !!token && !this.isTokenExpired(token);
-  }
-  isTokenExpired(token) {
-    try {
-      const decoded = decode(token);
-      if (decoded.exp < Date.now() / 1000) {
-        return true;
-      } else return false;
-    } catch (err) {
-      return false;
+const jwt = require('jsonwebtoken');
+
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
+
+module.exports = {
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    // ["Bearer", "<tokenvalue>"]
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
     }
-  }
-  getToken() {
-    // Retrieve token from localStorage
-    return localStorage.getItem("id_token");
-  }
-  login(idToken) {
-    // Saves user token to localStorage
-    localStorage.setItem("id_token", idToken);
-    window.location.assign("/");
-  }
-  logout() {
-    // Clear all for next time
-    localStorage.removeItem("id_token");
-    // page reset
-    window.location.assign("/");
-  }
-}
-export default new AuthService();
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
+  signToken: function ({ firstName, email, _id }) {
+    const payload = { firstName, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+};
